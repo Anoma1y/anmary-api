58b58d3791d2347112707cd26df2d05cae691847
German <ueuknjg@gmail.com>
2018-09-04 19:04:37 +0300

    fix product resource

--- app/Http/Resources/Product/Product.php
+++ app/Http/Resources/Product/Product.php
@@ -4,14 +4,28 @@ namespace App\Http\Resources\Product;
 
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Image\Image as ImageResource;
+use App\Http\Resources\Category\Category as CategoryResource;
+use App\Http\Resources\Brand\Brand as BrandResource;
+use App\Http\Resources\Season\Season as SeasonResource;
+
+use App\Models\Image\Image;
+use App\Models\Category\Category;
+use App\Models\Brand\Brand;
+use App\Models\Season\Season;
 
 class Product extends JsonResource {
     public function toArray($request) {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
+            'id' => (int)$this->id,
+            'name' => (string)$this->name,
             'description' => (string)$this->description,
+            'image' => new ImageResource(Image::find((int)$this->image_id)),
+            'category' => new CategoryResource(Category::find((int)$this->category_id)),
+            'brand' => new BrandResource(Brand::find((int)$this->brand_id)),
+            'season' => new SeasonResource(Season::find((int)$this->season_id)),
             'created_at' => (new Carbon($this->created_at))->getTimestamp(),
+            'updated_at' => (new Carbon($this->updated_at))->getTimestamp(),
         ];
     }
 }
